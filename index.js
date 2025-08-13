198575626409396f7bbe78b398e2db849b7335a8
Ryan Rosete <r.ryancedrick1993@gmail.com>
2025-08-14 00:43:58 +0800

    Add createUser and error exception

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+require("./database");
+
+const userRouter = require("./src/routes/user");
+
+app.use("/api/v1/user", userRouter);
+
 app.listen(5000, () => console.log("Server is running"));
